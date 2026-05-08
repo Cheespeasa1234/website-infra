f7e58e73adfcf70d9b54d4a9903ef5de78ab042e
Nate Levison <natelevison@gmail.com>
2026-05-08 14:49:27 -0400

    random fixes

--- setup.sh
+++ setup.sh
@@ -9,10 +9,9 @@ apt upgrade -y
 
 # setup firewall
 ufw allow OpenSSH
-ufw --force enable
 
 # clone all the things
-mkdir /www
+mkdir -p /www
 git clone https://github.com/Cheespeasa1234/natelevison.com.git /www/natelevison.com
 git clone https://github.com/Cheespeasa1234/static.natelevison.com.git /www/static.natelevison.com
 
@@ -22,20 +21,20 @@ git clone https://github.com/Cheespeasa1234/website-infra.git /tmp/website-infra
 # setup nginx
 apt install nginx -y
 ufw allow 'Nginx Full'
+ufw --force enable
 
 cp /tmp/website-infra/website-nginx/nginx.conf /etc/nginx/nginx.conf
-cp /tmp/website-infra/website-nginx/sites-available /etc/nginx -r
-rm /etc/nginx/sites-available default
-cp /tmp/website-infra/website-nginx/sites-enabled /etc/nginx -r
-rm /etc/nginx/sites-enabled default
+cp /tmp/website-infra/website-nginx/sites-available/*.conf /etc/nginx/sites-available/
+rm /etc/nginx/sites-available/default
+cp /tmp/website-infra/website-nginx/sites-enabled/*.conf /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
 
 # setup website
-apt install nodejs -y
-apt install npm -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt install -y nodejs
 
 cd /www/natelevison.com
 npm install -y
-npm install node
 npm run build
 
 # setup services

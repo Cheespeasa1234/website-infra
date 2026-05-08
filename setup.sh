f066eae37d209ae7bea33bed60edc77767a4c803
Nate Levison <natelevison@gmail.com>
2026-05-08 12:38:59 -0400

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ set -euo pipefail
 apt update
 apt upgrade -y
 
+# setup firewall
+ufw allow OpenSSH
+ufw enable
+
 # clone all the things
 mkdir /www
 git clone https://github.com/Cheespeasa1234/natelevison.com.git /www/natelevison.com
@@ -15,16 +19,28 @@ git clone https://github.com/Cheespeasa1234/static.natelevison.com.git /www/stat
 mkdir /tmp/website-infra
 git clone https://github.com/Cheespeasa1234/website-infra.git /tmp/website-infra
 
-# setup firewall
-ufw allow OpenSSH
-ufw enable
-
 # setup nginx
 apt install nginx -y
 ufw allow 'Nginx Full'
 
+cp /tmp/website-infra/website-nginx/nginx.conf /etc/nginx/nginx.conf
+cp /tmp/website-infra/website-nginx/sites-available /etc/nginx -r
+rm /etc/nginx/sites-available default
+cp /tmp/website-infra/website-nginx/sites-enabled /etc/nginx -r
+rm /etc/nginx/sites-enabled default
 
-
-# setup node
+# setup website
 apt install nodejs -y
 apt install npm -y
+
+cd /www/natelevison.com
+npm install -y
+npm install node
+npm run build
+
+# setup services
+cp /tmp/website-infra/website-system/*.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl restart nginx
+systemctl enable website.service
+systemctl start website.service
